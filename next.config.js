d8986fc16687681be2bd6340fc6a510e2793b641
Kazi Efazul Karim <efazul.fayad1717@gmail.com>
2024-03-01 02:15:44 +0600

    Update next.config.js with i18n and output settings

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH : "",
-  // images: {
-  //   loader: 'akamai',
-  //   path: process.env.NEXT_PUBLIC_URL,
-  // },  
-}
-
-module.exports = nextConfig
-
+module.exports = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  output: "standalone",
+};
