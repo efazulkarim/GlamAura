eeec656ee64baf950f092705a946b7a1d9d4abb1
Kazi Efazul Karim <efazul.fayad1717@gmail.com>
2024-02-29 15:23:17 +0600

    Add @datadog/browser-rum package

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/style.scss';
 import ColorSwitcher from '../common/elements/color-switcher/ColorSwitcher';
+import { datadogRum } from '@datadog/browser-rum';
 
 
+export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    datadogRum.init({
+      applicationId: '946d3d57-c2f1-40a4-aece-065dbd419e01',
+      clientToken: 'pubd04b086929aee9e7a57b62731d0f91ea',
+      site: 'us5.datadoghq.com',
+      service: 'glaumaura_blog',
+      env: process.env.NEXT_PUBLIC_DATADOG_ENV,
+      sessionSampleRate: 100,
+      sessionReplaySampleRate: 20,
+      trackUserInteractions: true,
+      trackResources: true,
+      trackLongTasks: true,
+      defaultPrivacyLevel: 'mask-user-input',
+    });
 
+    datadogRum.startSessionReplayRecording();
+  }, []);
 
-function MyApp({ Component, pageProps }) {
   return (
     <>
 
@@ -16,4 +33,4 @@ function MyApp({ Component, pageProps }) {
   )
 }
 
-export default MyApp
+
